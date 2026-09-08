edf4ab2c20353624a74e5b4b45c069747f29e48d
tejakakarla <tejakakarla11@gmail.com>
2026-09-08 10:05:04 +0530

    Implement string similarity calculation

---
+++ week6/stringSimilarity.java
@@ -0,0 +1,80 @@
+class Result {
+
+    public static long class Result {
+
+    public static long stringSimilarity(String s) {
+
+        int n = s.length();
+
+        int[] z = new int[n];
+
+        int left = 0;
+        int right = 0;
+
+        long answer = n;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i <= right) {
+                z[i] = Math.min(
+                    right - i + 1,
+                    z[i - left]
+                );
+            }
+
+            while (i + z[i] < n &&
+                   s.charAt(z[i]) ==
+                   s.charAt(i + z[i])) {
+
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > right) {
+                left = i;
+                right = i + z[i] - 1;
+            }
+
+            answer += z[i];
+        }
+
+        return answer;
+    }
+}
+(String s) {
+
+        int n = s.length();
+
+        int[] z = new int[n];
+
+        int left = 0;
+        int right = 0;
+
+        long answer = n;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i <= right) {
+                z[i] = Math.min(
+                    right - i + 1,
+                    z[i - left]
+                );
+            }
+
+            while (i + z[i] < n &&
+                   s.charAt(z[i]) ==
+                   s.charAt(i + z[i])) {
+
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > right) {
+                left = i;
+                right = i + z[i] - 1;
+            }
+
+            answer += z[i];
+        }
+
+        return answer;
+    }
+}
