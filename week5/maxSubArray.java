1f833f091d6a7cefea25cdfe246f03e52fd57f04
tejakakarla <tejakakarla11@gmail.com>
2026-09-08 09:51:49 +0530

    Update maxSubArray.java

--- week5/maxSubArray.java
+++ week5/maxSubArray.java
@@ -1,3 +1,26 @@
+BRUTE:
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+
+                sum += nums[j];
+
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+
+        return maxSum;
+    }
+}
+
+OPTIMIZED:
 class Solution {
     public int maxSubArray(int[] nums) {
 
