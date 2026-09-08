0411af85d0f63d853c901c3767cef78e112ca31b
tejakakarla <tejakakarla11@gmail.com>
2026-09-08 09:52:32 +0530

    Optimize birthday function for better performance
    Refactored birthday function to optimize performance by reducing nested loops and calculating the sum for the first window only.

--- week5/birthdaybar.java
+++ week5/birthdaybar.java
@@ -1,3 +1,33 @@
+BRUTE:
+class Result {
+
+    public static int birthday(
+            List<Integer> s,
+            int d,
+            int m) {
+
+        int count = 0;
+
+        for (int i = 0; i <= s.size() - m; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < i + m; j++) {
+                sum += s.get(j);
+            }
+
+            if (sum == d) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+
+
+OPTIMIZED:
 class Result {
 
     public static int birthday(
