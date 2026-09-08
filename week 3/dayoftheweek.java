0862fd7c63e99f200ad2ee3808e80556d4e9fcd5
tejakakarla <tejakakarla11@gmail.com>
2026-09-08 09:23:48 +0530

    Create dayoftheweek.java

---
+++ week 3/dayoftheweek.java
@@ -0,0 +1,37 @@
+PROBLEM STATEMENT:
+/* Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+
+Note: January 1, 1971 was a Friday.
+
+ 
+
+Example 1:
+
+Input: day = 31, month = 8, year = 2019
+Output: "Saturday"
+Example 2:
+
+Input: day = 18, month = 7, year = 1999
+Output: "Sunday"
+Example 3:
+
+Input: day = 15, month = 8, year = 1993
+Output: "Sunday"
+*/
+
+PROGRAM:
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        return date.getDayOfWeek()
+                   .getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+    }
+}
